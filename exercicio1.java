df06f395ff992883cf0bb851c0a540b7c509221d
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2024-01-22 14:20:46 -0300

    feat: first exercise

---
+++ exercicio1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class exercicio1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+
+        int numero = scanner.nextInt();
+
+        scanner.close();
+
+        if (numero % 2 == 0) {
+            System.out.println("O número é par.");
+        } else {
+            System.out.println("O número é ímpar.");
+        }
+
+        if (numero > 0) {
+            System.out.println("O número é positivo.");
+        } else if (numero < 0) {
+            System.out.println("O número é negativo.");
+        } else {
+            System.out.println("O número é zero.");
+        }
+    }
+}
